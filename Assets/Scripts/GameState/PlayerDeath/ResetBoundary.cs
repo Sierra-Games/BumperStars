522210e71945978983e5ad492c571244a18a6f14
Isaiah McLain <IsaiahMcLain@hotmail.com>
2023-05-01 19:23:17 -0700

    v2023.05.01 - When player hits lava lvl resets

---
+++ Assets/Scripts/GameState/PlayerDeath/ResetBoundary.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetBoundary : MonoBehaviour
+{
+    void OnTriggerEnter(Collider obj)
+    {
+        // Check if player is what collided with the lava 
+        if (obj.tag == "Player")
+        {
+            Debug.Log("here");
+            // If it was the player we load the loss/death screen and allow them to reset the level or retry 
+            // For now we just reload the level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        }
+    }
+}
